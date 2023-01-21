6b5629b2aa162200a05e52586f2b1edecd69abd8
Renz Landicho <renzrulandicho@gmail.com>
2023-01-21 21:29:12 +0800

    build: setup project for svelte

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import {
-  UI
-} from "./UI"
+import './app.css';
+import App from './App.svelte';
 
-UI.initialize();
+const app = new App({
+  target: document.getElementById('app'),
+});
+
+export default app;
---
+++ src/old/mainvanilla.js
@@ -0,0 +1,5 @@
+import {
+  UI
+} from "./UI"
+
+UI.initialize();
