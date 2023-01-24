0afc03002459dc14bedf01645f29207f14bcaf13
Renz Landicho <renzrulandicho@gmail.com>
2023-01-24 17:12:36 +0800

    feat: add Project component

---
+++ src/lib/Project.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Task from './Task.svelte';
+  import Modal from './Modal.svelte';
+
+  let activeTask: HTMLElement;
+
+  const setActiveTask = (task: HTMLElement) => {
+    activeTask = task;
+  };
+</script>
+
+<section class="project grid">
+  <Task {setActiveTask} />
+  <Modal {activeTask} />
+</section>
+
+<style lang="scss">
+  .project {
+    grid-column: 2;
+    gap: 0.2rem;
+  }
+</style>
