626df4b8dc65632de5135183785b5a37f88daa53
Renz Landicho <renzrulandicho@gmail.com>
2023-02-03 09:59:40 +0800

    refactor: refactor for readability

--- src/lib/Animate.ts
+++ src/lib/Animate.ts
@@ -1,16 +1,13 @@
 
 class Animate {    
       static swapPriority(priority: string): string {
-        switch (priority) {
-          case 'Normal': 
-             return 'var(--priority-normal)';
-          case 'Important':
-            return 'var(--priority-important)';
-          case 'Urgent':
-            return 'var(--priority-urgent)';
-          default:
-            return 'var(--priority-default)';
+        const priorities = {
+          Normal: 'var(--priority-normal)',
+          Important: 'var(--priority-important)',
+          Urgent: 'var(--priority-urgent)'
         }
+
+        return priorities[priority] || 'var(--priority-default)';
       }
 }
 
